4a50bf30cf4ba17131d8ad568ee6d082a8a73423
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-28 14:26:34 +0300

    hw

--- src/main/java/pro/sky/skyprogenericsmaphw/exception/exception/EmployeeNotFoundException.java
+++ src/main/java/pro/sky/skyprogenericsmaphw/exception/exception/EmployeeNotFoundException.java
@@ -1,4 +1,8 @@
 package pro.sky.skyprogenericsmaphw.exception.exception;
 
-public class EmployeeNotFoundException extends Exception{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmployeeNotFoundException extends RuntimeException{
 }
--- src/main/java/pro/sky/skyprogenericsmaphw/service/service/EmployeeService.java
+++ src/main/java/pro/sky/skyprogenericsmaphw/service/service/EmployeeService.java
@@ -1,5 +1,16 @@
 package pro.sky.skyprogenericsmaphw.service.service;
 
+import pro.sky.skyprogenericsmaphw.model.Employee;
+
+import java.util.Collection;
+
 public interface EmployeeService  {
 
+    Employee add(String firsName, String lastName);
+
+    Employee remove(String firsName, String lastName);
+
+    Employee find(String firsName, String lastName);
+
+    Collection<Employee> findAll();
 }
