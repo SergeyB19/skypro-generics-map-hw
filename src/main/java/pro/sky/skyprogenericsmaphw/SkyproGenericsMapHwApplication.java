2327635bc8b59899d99cca9c3344c9f5eb4d95ab
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-28 12:10:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprogenericsmaphw/SkyproGenericsMapHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprogenericsmaphw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproGenericsMapHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproGenericsMapHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprogenericsmaphw/SkyproGenericsMapHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprogenericsmaphw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproGenericsMapHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
